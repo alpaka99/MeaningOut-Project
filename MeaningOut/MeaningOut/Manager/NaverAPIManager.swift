26f45ed1c2a249854f24dd91e04632c8f0cce04c
Lets <ruvan123@naver.com>
2024-07-01 17:45:14 +0900

    Refactor: Build Router Pattern for URLRequest

--- MeaningOut/MeaningOut/Manager/NaverAPIManager.swift
+++ MeaningOut/MeaningOut/Manager/NaverAPIManager.swift
@@ -5,39 +5,103 @@
 //  Created by user on 6/23/24.
 //
 import Foundation
+import UIKit
 
 import Alamofire
 import Kingfisher
-import UIKit
 
 final class NaverAPIManager {
     static let shared = NaverAPIManager()
     
-    private init() {
+    private init() { }
+    
+    internal func fetchNaverShoppingResponse(_ searchText: String, start: Int = 1, sortOption: String ,completionHandler: @escaping (NaverShoppingResponse) -> ()) {
+//            let parameters: Parameters = [
+//                ParameterKey.query : searchText,
+//                ParameterKey.display : PageNationConstants.pageAmount,
+//                ParameterKey.start : start,
+//                ParameterKey.sort : sortOption
+//            ]
+            
+//            let url = APIKey.Naver.shoppingURL
+            
+//            AF.request(
+//                url,
+//                parameters: parameters,
+//                headers: APIKey.Naver.headers
+//            ).responseDecodable(of: NaverShoppingResponse.self) { response in
+//                switch response.result {
+//                case .success(let value):
+//                    completionHandler(value)
+//                case .failure(let error):
+//                    print(error)
+//                }
+//        }
+        
+        guard let testURL = URLRouter.naverShopping("으앙", 1, .simularity).urlRequest else { return }
+        print(testURL.url?.absoluteString)
+        URLSession.shared.dataTask(with: testURL) { data, response, error in
+            print(data)
+
+            print(response)
+            
+            print(error)
+        }.resume()
+        
         
     }
     
-    internal func fetchNaverShoppingResponse(_ searchText: String, start: Int = 1, sortOption: String ,completionHandler: @escaping (NaverShoppingResponse) -> ()) {
-            let parameters: Parameters = [
-                ParameterKey.query : searchText,
-                ParameterKey.display : PageNationConstants.pageAmount,
-                ParameterKey.start : start,
-                ParameterKey.sort : sortOption
-            ]
+    enum URLRouter {
+        case naverShopping(String, Int, SortOptions)
+        
+        var baseURL: String {
+            switch self {
+            case .naverShopping(_, _, _):
+                return APIKey.Naver.shoppingURL
+            }
+        }
+        
+        var urlComponents: URLComponents {
+            var components = URLComponents()
+            components.scheme = "https"
+            components.path = self.baseURL
             
-            let url = APIKey.Naver.shoppingURL
             
-            AF.request(
-                url,
-                parameters: parameters,
-                headers: APIKey.Naver.headers
-            ).responseDecodable(of: NaverShoppingResponse.self) { response in
-                switch response.result {
-                case .success(let value):
-                    completionHandler(value)
-                case .failure(let error):
-                    print(error)
-                }
+            switch self {
+            case .naverShopping(let searchText, let start, let sortOption):
+                components.queryItems = [
+                    URLQueryItem(name: "method", value: "get"),
+                    URLQueryItem(name: "query", value: searchText),
+                    URLQueryItem(name: "start", value: String(start)),
+                    URLQueryItem(name: "sort", value: sortOption.rawValue)
+                ]
+            }
+            
+            return components
+        }
+        
+//        var headers: HTTPHeaders {
+//            switch self {
+//            case .naverShopping(_, _, _):
+//                return APIKey.Naver.headers
+//            }
+//        }
+        
+        var headers: [String:String] {
+            switch self {
+            case .naverShopping(_, _, _):
+                return APIKey.Naver.headers
+            }
+        }
+        
+        
+        var urlRequest: URLRequest? {
+            guard let url = self.urlComponents.url else { return nil }
+            var urlRequest = URLRequest(url: url)
+            
+            urlRequest.allHTTPHeaderFields = self.headers
+            
+            return urlRequest
         }
     }
 }
